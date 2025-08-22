f5195e342f8ceb778d480912dca24046372cf4e7
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-08-22 15:27:32 +0300

    Create signals.py for auto creating profile for new user and add it to apps.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
-from accounts.models import Profile
-
-
-User = get_user_model()
+from accounts.models import User, Profile
 
 
 @admin.register(User)
