ee337f62543ecdab27fe6fa942ed97b5fd7521a2
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-08-22 14:24:00 +0300

    Created base admin register to apps accounts, bookings, books and created superuser

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from books.models import Author, Genre, BookListing
+
+
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookListing)
